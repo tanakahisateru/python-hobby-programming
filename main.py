b8bb5966e04e20091a60c266c8c3c97a7d59cdd0
Hisateru Tanaka <tanakahisateru@gmail.com>
2023-07-15 00:27:29 +0900

    tkinterに型

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-from tkinter import *
+from typing import Any
+from collections.abc import Callable
+
+import tkinter
 from tkinter import ttk
 
 
-def make_frame(root: Tk):
+def make_frame(root:tkinter.Tk, on_quit: Callable[[], Any]):
     """
     Tkのフレームをつくります
     """
     frm = ttk.Frame(root, padding=10)
-    frm.grid()
-    ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=1)
+    frm.grid(padx=10, pady=10)
+
+    ttk.Label(
+        frm,
+        text="Hello World!",
+        font=("Helvetica", 20, "bold"),
+        # foreground="#ff0000",
+    ).grid(column=0, row=0, pady=20)
+
+    ttk.Button(
+        frm,
+        text="Quit",
+        command=on_quit
+    ).grid(column=0, row=1)
 
 
 if __name__ == '__main__':
-    root = Tk()
-    make_frame(root)
+    root = tkinter.Tk()
+    make_frame(root, root.destroy)
     root.mainloop()
