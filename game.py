0a34edfb02d6a47b049119302754e6b4b3403ef3
Hisateru Tanaka <tanakahisateru@gmail.com>
2023-07-15 01:35:24 +0900

    ゲームに動きをつけた

--- game.py
+++ game.py
@@ -1,12 +1,29 @@
+import math
 import pyxel
 
 
-def add(a, b):
-    return a + b
+class Game:
+    frame: int
+
+    def __init__(self):
+        self.frame = 0
+
+    def update(self):
+        self.frame += 1
+
+    def draw(self):
+        pyxel.cls(1)
+        r = time_to_radius(self.frame, 30, 40)
+        pyxel.circb(60, 60, r, 7)
+
+
+def time_to_radius(frame:int, cycle:int, radius:int) -> int:
+    t = 2.0 * math.pi * float(frame) / float(cycle)
+    r_half = float(radius) / 2.0
+    return round(r_half * math.sin(t) + r_half)
 
 
 if __name__ == '__main__':
     pyxel.init(120, 120)
-    pyxel.cls(1)
-    pyxel.circb(60, 60, 40, 7)
-    pyxel.show()
+    game = Game()
+    pyxel.run(game.update, game.draw)
--- test_game.py
+++ test_game.py
@@ -1,6 +1,7 @@
 from game import *
 
 
-def test_add():
-    assert add(1, 2) == 3
-
+def test_time_to_radius():
+    assert time_to_radius(0, 30, 40) == 20
+    assert time_to_radius(15, 30, 40) == 20
+    assert time_to_radius(30, 30, 40) == 20
